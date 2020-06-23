8dc2029ec352399a5b6366e898599a0eb456660d
Valera <valera@example.com>
2020-06-23 10:29:52 +0300

    add

--- app/src/main/java/com/hfad/weatherapp/MainActivity.java
+++ app/src/main/java/com/hfad/weatherapp/MainActivity.java
@@ -1,11 +1,17 @@
 package com.hfad.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,15 +22,58 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     TextView txtCity, txtLastUpdate, txtDescription, txtHumidity, txtTime, txtCelsius;
     ImageView mImageView;
 
-    LocationManager mLocationManager;
+    LocationManager mLocationManager; // класс , который получает доступ к системной службе(датчики телефона, GPS и т.д)
     String provider;
     static double lat, lng;
     OpenWeatherMap mOpenWeatherMap = new OpenWeatherMap();
+
+    int MY_PERMISSION = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtCity = findViewById(R.id.txtCity);
+        txtLastUpdate = findViewById(R.id.txtLastUpdate);
+        txtDescription = findViewById(R.id.txtDescription);
+        txtHumidity = findViewById(R.id.txtHumidity);
+        txtTime = findViewById(R.id.txtTime);
+        txtCelsius = findViewById(R.id.txtCelsius);
+        mImageView = findViewById(R.id.imageView);
+
+        // Get lat and lon
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE); // получаем ссылку на LocationManager и передаем тип службы, которая нам нужна
+        // Если GPS не доступно , то с помощью данного метода выбирается лучший поставшик услуг
+        // класс Criteria исп для опр наиболее подходящего провайдера местоположения, в случаи доступности нескольких провайдеров
+        provider = mLocationManager.getBestProvider(new Criteria(), false);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                    Manifest.permission.INTERNET,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.SYSTEM_ALERT_WINDOW,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+
+            }, MY_PERMISSION);
+        }
+        Location location = mLocationManager.getLastKnownLocation(provider); // дает доступ к данным о географическом положении последнем
+
+        if(location == null){
+            Log.e("TAG", "NO Location");
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 
     @Override
