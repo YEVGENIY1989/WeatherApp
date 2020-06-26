2773d8560a616f4c09ab2c0a3235343c420c1ea5
Valera <valera@example.com>
2020-06-26 15:47:26 +0300

    add

--- app/src/main/java/com/hfad/weatherapp/MainActivity.java
+++ app/src/main/java/com/hfad/weatherapp/MainActivity.java
@@ -4,24 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.drm.ProcessedData;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.hfad.weatherapp.Helper.Helper;
 import com.hfad.weatherapp.Model.OpenWeatherMap;
+
+import java.lang.reflect.Type;
 
 public class MainActivity extends AppCompatActivity implements LocationListener {
 
     TextView txtCity, txtLastUpdate, txtDescription, txtHumidity, txtTime, txtCelsius;
     ImageView mImageView;
-
+    boolean b = false;
     LocationManager mLocationManager; // класс , который получает доступ к системной службе(датчики телефона, GPS и т.д)
     String provider;
     static double lat, lng;
@@ -60,7 +69,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         }
         Location location = mLocationManager.getLastKnownLocation(provider); // дает доступ к данным о географическом положении последнем
 
-        if(location == null){
+        if (location == null) {
             Log.e("TAG", "NO Location");
         }
 
@@ -69,15 +78,39 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     @Override
     protected void onPause() {
         super.onPause();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                    Manifest.permission.INTERNET,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.SYSTEM_ALERT_WINDOW,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+
+            }, MY_PERMISSION);
+        }
+        mLocationManager.removeUpdates(this);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                    Manifest.permission.INTERNET,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.SYSTEM_ALERT_WINDOW,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+
+            }, MY_PERMISSION);
+        }
+        mLocationManager.requestLocationUpdates(provider, 400, 1, this);
     }
 
     @Override
     public void onLocationChanged(Location location) {
+        lat = location.getLatitude();
+        lng = location.getLongitude();
 
     }
 
@@ -94,5 +127,40 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     @Override
     public void onProviderDisabled(String s) {
 
+    }
+
+    private class GetWeather extends AsyncTask<String, Void, String>{
+        ProgressDialog pd = new ProgressDialog(MainActivity.this);
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pd.setTitle("Please wait...");
+            pd.show();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+           String stream = null;
+           String utlString = strings[0];
+
+            Helper http = new Helper();
+            stream = http.getHTTPData(utlString);
+            return stream;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if(s.contains("Error: Not found city")){
+                pd.dismiss();
+            }
+            Gson gson = new Gson();
+            Type mType = new TypeToken<OpenWeatherMap>(){}.getType();
+            mOpenWeatherMap = gson.fromJson(s, mType);
+            pd.dismiss();
+
+
+        }
     }
 }
